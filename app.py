c7bb3a00a49e03ae9af1be47b797a509c4e2ac7c
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2023-10-05 16:24:37 -0500

    working copy

---
+++ app.py
@@ -0,0 +1,16 @@
+import prompter
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def prompt():
+    prompt = request.form['prompt']
+    response = prompter.chatgpt(prompt)
+    return render_template('/submit.html',
+                           prompt = prompt,
+                           response = response)    
---
+++ prompter.py
@@ -0,0 +1,14 @@
+import semantic_kernel as sk
+from semantic_kernel.connectors.ai.open_ai import OpenAIChatCompletion
+
+def chatgpt(prompt):
+    kernel = sk.Kernel()
+    api_key, org_id = sk.openai_settings_from_dot_env()
+    kernel.add_text_completion_service("dv", OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id))
+    with open('resume.txt', encoding='utf-8') as f:
+        lines = f.readlines()
+    whole_input = ' '.join(lines) + ' ' + prompt
+    semantic_function = kernel.create_semantic_function(whole_input)
+    response = semantic_function()
+    return response
+
